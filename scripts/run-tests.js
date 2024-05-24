dbe79be5cbde303cdec0e157739ffad3b2f3231d
juli <julisahun@gmail.com>
2024-05-24 15:54:42 +0200

    update script

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -2,17 +2,27 @@ const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
 
 const testFiles = {
-  analyzer: ["simple", "condition", "loops", "functions", "scopes", "properties", "preprocess"],
+  analyzer: ["simple", "condition", "loops", "functions", "scopes", "properties", "preprocess"]
   // formatter: ["variables", "functions", "scopes", "includes"],
 };
+
+async function runTest(test) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await exec(`npm test ${test}`, { stdio: "inherit" })
+      console.log(`Test ${test} passed!`)
+      resolve()
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
 
 const runTests = async () => {
   for (module in testFiles) {
     const moduleTests = testFiles[module];
     const results = await Promise.allSettled(
-      moduleTests.map(async (testFile) =>
-        exec(`npm test ${module}/${testFile}`, { stdio: "inherit" }),
-      ),
+      moduleTests.map(test => runTest(`${module}/${test}`))
     );
     const failedTests = results
       .map((result, index) => ({ ...result, test: moduleTests[index] }))
